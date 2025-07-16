22f54d69a1cf45b8f36cc89a75d14e327ad78136
goga574 <discordforgoa@gmail.com>
2025-07-16 11:12:40 +0400

    1

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-let h1 = document.getElementsByTagName("h1")
-let section = document.getElementsByClassName("input-section")
-let td = document.querySelectorAll("td")
-let th = document.querySelectorAll("th")
-function darkMode(){
-    h1[0].style.color = "white"
-    section[0].style.backgroundColor="black"
-    for(let i of td){
-        i.style.color="white"
-    }
-    for(let i of th){
-        i.style.color="white"
-    }
-    
-}
+let seeHotels = document.querySelector(".seehotels")
+let body = document.body
+let navHotel = document.querySelectorAll("li")
+let blurDiv = document.querySelector(".blurDiv")
+let buttonX = document.querySelector(".fa-x")
 
-function lightMode(){
-    h1[0].style.color = "black"
-    section[0].style.backgroundColor="white"
-    for(let i of td){
-        i.style.color="black"
-    }
-    for(let i of th){
-        i.style.color="black"
-    }
-}
+seeHotels.addEventListener("click",()=>{
+    blurDiv.style.display = "flex"
+    
+})
+navHotel[0].addEventListener("click",()=>{
+    blurDiv.style.display = "flex"
+})
+
+buttonX.addEventListener("click" , ()=>{
+    blurDiv.style.display = "none"
+})
+
